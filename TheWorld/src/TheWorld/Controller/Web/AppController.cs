2b39033b68117afab9a229d65b75136912e5528a
IsItBroken <zlipps1@gmail.com>
2016-09-28 23:11:08 -0400

    After MVC 6

---
+++ TheWorld/src/TheWorld/Controller/Web/AppController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controller.Web
+{
+    public class AppController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly IMailService _mailService;
+        private readonly IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel viewModel)
+        {
+            if(viewModel.Email.Contains("aol.com")) ModelState.AddModelError("", "We don't support aol addresses");
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], viewModel.Email, "From TheWorld", viewModel.Message);
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -1,21 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TheWorld.Services;
 
 namespace TheWorld
 {
     public class Startup
     {
+        private readonly IHostingEnvironment _environment;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment environment)
+        {
+            _environment = environment;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_environment.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
+
+            if (_environment.IsDevelopment())
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else
+            {
+                // Implement a real Mail Service
+            }
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,9 +51,15 @@ namespace TheWorld
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseDefaultFiles();
+            
             app.UseStaticFiles();
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new {controller = "App", action = "Index"});
+            });
         }
     }
 }
